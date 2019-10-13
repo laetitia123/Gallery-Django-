fdd7bf75a3bf93836a7a58e8f9675860760ff09f
laetitia123 <uwizelaetitia@gmail.com>
2019-10-13 14:46:30 +0200

    styling

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
    
-    url(r'^$',views.gallery,name='newsToday'),
+    url(r'^$',views.gallery,name='galleryToday'),
     url(r'^search/', views.search_results, name='search_results'),
     
     url(r'^image/(\d+)',views.image,name ='image'),
