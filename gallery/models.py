d327172776a39f6cfaba0b73f726420fa6514805
laetitia123 <uwizelaetitia@gmail.com>
2019-10-13 10:03:38 +0200

    test forder added for testing

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
@@ -14,26 +15,62 @@ class Category(models.Model):
          return self.category
     class Meta:
         ordering = ['category']
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+    
+  
+
+
 class Location(models.Model):
     location= models.CharField(max_length =30)
     
-    
-   
     def __str__(self):
          return self.location
     class Meta:
         ordering = ['location']
-class Image (models.Model):
-    post = models.TextField()
-    category = models.ForeignKey(Category ,db_column="category")
+    def save_location(self):
+        self.save()
+    
+    def delete_location(self):
+        self.delete()
+    
+    def update_location(self, update):
+        self.location = update
+        self.save()  
 
+    @classmethod
+    def get_location_id(cls, id):
+        locate = Location.objects.get(pk = id)
+        return locate
+
+
+class Image (models.Model):
+
+    name= models.CharField(max_length =30)
+    description= models.TextField()
+    category = models.ForeignKey(Category)
     location= models.ForeignKey(Location)
     pub_date = models.DateTimeField(auto_now_add=True)
     image_image = models.ImageField(upload_to = 'images/')
 
+    def __str__(self):
+        return self.name  
+
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
     @classmethod
     def get_all_images(cls):
         image = cls.objects.all()
+        return image 
+  
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = Image.objects.get(id=id)
         return image
     @classmethod
     def search_by_category(cls,search_term):
@@ -46,5 +83,3 @@ class Image (models.Model):
        return images 
      
        
-
-    
