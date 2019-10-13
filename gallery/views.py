dc9678a52b35b1405b3ddce9e59c3c99b06e2e3d
laetitia123 <uwizelaetitia@gmail.com>
2019-10-13 15:23:00 +0200

    styling

--- gallery/views.py
+++ gallery/views.py
@@ -37,38 +37,7 @@ def image(request,image_id):
         raise Http404()
     return render(request,"all-gallery/image.html", {"image":image})
 def filter_by_location(request,location_id):
-   """
-   Function that filters images by location
-   """
+   
    images = Image.filter_by_location(id=location_id )
    return render (request,"all-gallery/location.html", {"images":images})
 
-
-# def single_image(request,image_id):
-#     image=Image.objects.get(id = image_id)
-#     return render(request, 'image.html',{"image":image})
-
-def convert_dates(dates):
-
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
-def past_days_news(request, past_date):
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-        assert False
-
-    if date == dt.date.today():
-        return redirect(news_today)
-
-    news = Image.days_news(date)
-    return render(request, 'all-news/past-news.html',{"date": date,"news":news})
