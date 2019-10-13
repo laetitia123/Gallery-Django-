b0d39680e776f33b046f9c789d93fe731fa17f06
laetitia123 <uwizelaetitia@gmail.com>
2019-10-13 10:06:52 +0200

    intanstance added

--- gallery/tests.py
+++ gallery/tests.py
@@ -10,4 +10,12 @@ class ImageTestClass(TestCase):
         self.people = Category(category='people')
         self.people.save_category()
 
-     
+        self.kigali = Location(location='kigali')
+        self.kigali.save_location()
+
+        self.image = Image(name='amezing', description='group', location=self.kigali, category=self.people)
+        self.image.save_image()
+   def test_instance(self):
+       self.assertTrue(isinstance(self.image, Image))
+
+ 
